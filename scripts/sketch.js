449e3a8c326145659741c3740a5dc96d6b5b0ae0
Jasen Wright <jasen.g.wright@gmail.com>
2017-01-31 00:36:19 -0800

    Implemented 'Clear' and 'Resize' buttons

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,11 +1,39 @@
 $(document).ready(function() {
-	for (var i = 0; i < 16; i++) {
-		for (var j = 0; j < 16; j++) {
+	createGrid(16);
+	clearCanvas();
+	drawCanvas();
+	sizeCanvas();
+});
+
+function createGrid(size) {
+	$("#main").children().remove();
+	for (var i = 0; i < size; i++) {
+		for (var j = 0; j < size; j++) {
 			var $div = $("<div class='unit'></div>");
-			$div.hover(function(){
-				$(this).addClass('hov');
-			});
+			$div.css("height", 100/size + "%");
+			$div.css("width", 100/size + "%");
 			$div.appendTo('#main');
 		}
 	}
-});
+	drawCanvas();
+};
+
+function clearCanvas() {
+	$("#clear").click(function() {
+		$(".unit").css("background-color", "white");
+	});
+}
+
+function drawCanvas() {
+	$(".unit").hover(function(){
+		$(this).css("background-color", "black");
+	});
+}
+
+function sizeCanvas() {
+	$("#resize").click(function() {
+		var user = prompt("Select a new size for grid");
+		var newSize = parseInt(user);
+		createGrid(newSize);
+	});
+}
